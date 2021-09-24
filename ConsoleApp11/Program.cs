8e8de7285599deeeb207c87b4e6afe432fac3704
Szymon <Szymon@LAPTOP-2DIS65OF>
2021-09-24 21:10:08 +0200

    Add project files.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = 0;
+            while (true && a <=100)
+            {
+                bool czyPierwsza = true;
+                for (int i = 2; i < a; i++)
+                {
+                    if (a % i == 0)
+                    {
+                        czyPierwsza = false;
+                    }
+
+                }    
+                    
+                if (czyPierwsza)
+                   
+                    
+                {
+
+                    Console.WriteLine(a);
+                    
+                    
+                }
+             
+                
+                
+                a++;
+            }
+
+
+        }
+    }
+}
